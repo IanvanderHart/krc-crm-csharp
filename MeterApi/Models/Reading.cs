4ec2af982b94f5b68a0d9268d74ec34b56c2b2ad
IanvanderHart <im0606@mail.ru>
2026-08-25 08:01:46 +0700

    Restore Reading.cs with JSON property names

--- MeterApi/Models/Reading.cs
+++ MeterApi/Models/Reading.cs
@@ -1,22 +1,32 @@
 using System.Text.Json.Serialization;
 
+namespace MeterApi.Models;
+
 public class Reading
 {
     public int Id { get; set; }
+
     [JsonPropertyName("readingDate")]
     public string ReadingDate { get; set; } = string.Empty;
+
     [JsonPropertyName("locality")]
     public string Locality { get; set; } = string.Empty;
+
     [JsonPropertyName("street")]
     public string? Street { get; set; }
+
     [JsonPropertyName("house")]
     public string House { get; set; } = string.Empty;
+
     [JsonPropertyName("apartment")]
     public string? Apartment { get; set; }
+
     [JsonPropertyName("xvsCurrent")]
     public decimal XVS_Current { get; set; }
+
     [JsonPropertyName("gvsCurrent")]
     public decimal? GVS_Current { get; set; }
+
     [JsonPropertyName("note")]
     public string? Note { get; set; }
 }
